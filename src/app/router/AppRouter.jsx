e5efbdae2490cf1c707a4aa13e4acafb5daea760
passerajose <espinoza.esp123@gmail.com>
2026-04-08 19:46:04 -0300

    feat: implement primereact modal dialog interface

--- src/app/router/AppRouter.jsx
+++ src/app/router/AppRouter.jsx
@@ -12,51 +12,59 @@ import EmojiSelector from "../../features/emoji-selector/components/EmojiSelecto
 import Weather from "../../features/weather/components/Weather.jsx";
 import { BookProvider } from "../../features/book-library/hooks/BookContext.jsx";
 import SearchBoxFilter from "../../features/search-box-filter/components/SearchBoxFilter.jsx";
+import ModalHome from "../../features/modal-dialog/ModalHome.jsx";
 
 function AppRouter() {
-    // Para mensajes Toast
-    const toast = useRef(null);
-    const showMessage = useCallback((sev, sum, det) => {
+  // Para mensajes Toast
+  const toast = useRef(null);
+  const showMessage = useCallback((sev, sum, det) => {
     toast.current?.show({
-        severity: sev,
-        summary: sum,
-        detail: det,
-        life: 4000,
+      severity: sev,
+      summary: sum,
+      detail: det,
+      life: 4000,
     });
-    }, []);
+  }, []);
   return (
     <BrowserRouter>
-    <BookProvider>
-      <Toast ref={toast} />
-      <Routes element={<MainLayout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
-        <Route
-          path="/pomodoro"
-          element={<Pomodoro showMessage={showMessage} />}
-        />
-        <Route
-          path="/weather"
-          element={<Weather showMessage={showMessage} />}
-        />
-        <Route
-          path="/emoji-selector"
-          element={<EmojiSelector showMessage={showMessage} />}
-        />
-        <Route
-          path={"/book-library"}
-          element={<BookLibraryHome showMessage={showMessage} />}
-        />
-        <Route
-          path="/book-library/home"
-          element={<BookLibraryHome showMessage={showMessage} />}
-        />
-        <Route
-          path="/book-library/create"
-          element={<BookLibraryCreate showMessage={showMessage} />}
-        />
-        <Route path="/search-box-filter" element={<SearchBoxFilter showMessage={showMessage}/>}/>
-      </Routes>
+      <BookProvider>
+        <Toast ref={toast} />
+        <Routes element={<MainLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route
+            path="/pomodoro"
+            element={<Pomodoro showMessage={showMessage} />}
+          />
+          <Route
+            path="/weather"
+            element={<Weather showMessage={showMessage} />}
+          />
+          <Route
+            path="/emoji-selector"
+            element={<EmojiSelector showMessage={showMessage} />}
+          />
+          <Route
+            path={"/book-library"}
+            element={<BookLibraryHome showMessage={showMessage} />}
+          />
+          <Route
+            path="/book-library/home"
+            element={<BookLibraryHome showMessage={showMessage} />}
+          />
+          <Route
+            path="/book-library/create"
+            element={<BookLibraryCreate showMessage={showMessage} />}
+          />
+          <Route
+            path="/search-box-filter"
+            element={<SearchBoxFilter showMessage={showMessage} />}
+          />
+          <Route
+            path="/modal-dialog"
+            element={<ModalHome showMessage={showMessage} />}
+          />
+        </Routes>
       </BookProvider>
     </BrowserRouter>
   );
--- src/features/home/components/Navbar.jsx
+++ src/features/home/components/Navbar.jsx
@@ -4,33 +4,53 @@ import { MdHome, MdOutlineEmojiEmotions } from "react-icons/md";
 import { FaBookBible, FaClock } from "react-icons/fa6";
 import { SiAccuweather } from "react-icons/si";
 import { RiSearchFill } from "react-icons/ri";
+import { SiDialogflow } from "react-icons/si";
 
 const Navbar = () => {
   return (
     <div className="flex flex-row justify-center gap-6 p-4 bg-gray-200 text-xl">
       <div className="flex items-center gap-2">
-        <MdHome size={23} />
-        <Link to="/home">Home</Link>
+        <Link className="flex flex-row items-center gap-2" to="/home">
+          <MdHome size={23} />
+          Home
+        </Link>
       </div>
       <div className="flex items-center gap-2">
-        <FaClock size={17} />
-        <Link to="/pomodoro">Pomodoro</Link>
+        <Link className="flex flex-row items-center gap-2" to="/pomodoro">
+          <FaClock size={17} />
+          Pomodoro
+        </Link>
       </div>
       <div className="flex items-center gap-2">
-        <SiAccuweather size={19} />
-        <Link to="/weather">Weather</Link>
+        <Link className="flex flex-row items-center gap-2" to="/weather">
+          <SiAccuweather size={19} /> Weather
+        </Link>
       </div>
       <div className="flex items-center gap-2">
-        <MdOutlineEmojiEmotions size={22} />
-        <Link to="/emoji-selector">Emoji Selector</Link>
+        <Link className="flex flex-row items-center gap-2" to="/emoji-selector">
+          <MdOutlineEmojiEmotions size={22} />
+          Emoji Selector
+        </Link>
       </div>
       <div className="flex items-center gap-2">
-        <FaBookBible size={19} />
-        <Link to="/book-library">Book Library</Link>
+        <Link className="flex flex-row items-center gap-2" to="/book-library">
+          <FaBookBible size={19} />
+          Book Library
+        </Link>
       </div>
       <div className="flex items-center gap-2">
-        <RiSearchFill size={22} />
-        <Link to="/search-box-filter">Search Box Filter</Link>
+        <Link
+          className="flex flex-row items-center gap-2"
+          to="/search-box-filter"
+        >
+          <RiSearchFill size={22} />
+          Search Box Filter
+        </Link>
+      </div>
+      <div className="flex items-center gap-2">
+        <Link className="flex flex-row items-center gap-2" to="/modal-dialog">
+          <SiDialogflow size={22} /> Modal Dialog
+        </Link>
       </div>
     </div>
   );
